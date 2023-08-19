51e3af9975232e9e3db19b032f853f71b5835424
vinicidesu <viniciusmoraeslima@gmail.com>
2023-08-19 14:33:18 -0300

    FEAT: EF configuration

---
+++ src/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperPets.Models.Animal;
+
+namespace SuperPets.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Animal> Animals { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Animal>();
+
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
---
+++ src/Data/EntityConfiguration/AnimalEntityTypeConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SuperPets.Models.Animal;
+
+namespace SuperPets.Data.EntityConfiguration
+{
+    public class AnimalEntityTypeConfiguration : IEntityTypeConfiguration<Animal>
+    {
+        public void Configure(EntityTypeBuilder<Animal> builder)
+        {
+            builder
+                .HasKey(p => p.Id);
+
+            builder
+                .Property(p => p.Name)
+                .IsRequired()
+                .HasColumnType("varchar(30)");
+
+            builder
+                .Property(p => p.Description)
+                .HasColumnType("varchar(200)");
+
+            builder
+                .Property(p => p.Species)
+                .IsRequired()
+                .HasColumnType("varchar(20)");
+
+            builder
+                .Property(p => p.Gender)
+                .IsRequired()
+                .HasColumnType("varchar(10)");
+
+            builder
+                .Property(p => p.Age)
+                .IsRequired();
+
+            builder
+                .Property(p => p.Size)
+                .IsRequired();
+
+            builder
+                .ToTable("Animals");
+        }
+    }
+}
