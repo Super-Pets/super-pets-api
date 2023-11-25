8df3a6ad7c33397dcc1886910e0c548d7dfa18c0
vinicidesu <viniciusmoraeslima@gmail.com>
2023-11-25 19:09:13 -0300

    Fix: Put corrigido

--- src/Endpoints/Animals/AnimalPut.cs
+++ src/Endpoints/Animals/AnimalPut.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using SuperPets.Data;
 using SuperPets.Domain.Animals;
+using System.Drawing;
+using System.Reflection;
 
 namespace SuperPets.Endpoints.Animals
 {
@@ -10,16 +14,45 @@ namespace SuperPets.Endpoints.Animals
         public static string[] Methods => new string[] { HttpMethod.Put.ToString() };
         public static Delegate Handle => Action;
 
-        public static IResult Action([FromRoute] int id, [FromBody] AnimalRequest animalRequest, AppDbContext context)
+        public static IResult Action([FromRoute] int id, AnimalRequest animalRequest, AppDbContext context)
         {
             var animal = context.Animals.Find(id);
 
-            if(animal != null)
-            {
-                context.Animals.Update(animalRequest);
-                var result = context.SaveChanges();
-            }
+            if (animal is null) return Results.NotFound();
+
+            if(!animalRequest.Name.IsNullOrEmpty())
+                animal.Name = animalRequest.Name;
+
+            if (!animalRequest.Description.IsNullOrEmpty())
+                animal.Description = animalRequest.Description;
+
+            if (!animalRequest.Species.IsNullOrEmpty())
+                animal.Species = animalRequest.Species;
+
+            if (!animalRequest.Gender.IsNullOrEmpty())
+                animal.Gender = animalRequest.Gender;
+
+            if (animalRequest.Age != 0)
+                animal.Age = animalRequest.Age;
+
+            if (!animalRequest.Size.IsNullOrEmpty())
+                animal.Size = animalRequest.Size;
+
+            if (!animalRequest.Local.IsNullOrEmpty())
+                animal.Local = animalRequest.Local;
+
+            if (!animalRequest.Vaccines.IsNullOrEmpty())
+                animal.Vaccines = animalRequest.Vaccines;
+
+            animal.Castration = animalRequest.Castration;
+            animal.UpdatedBy = animalRequest.UpdatedBy;
+            animal.UpdatedOn = DateTime.Now;
+
+
+            context.Animals.Update(animalRequest);
+            context.SaveChanges();
+
             return Results.Ok();
         }
-    }
+}
 }
